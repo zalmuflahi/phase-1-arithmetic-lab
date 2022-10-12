0ca8a10a67d8622330cc2e50a69b14e8e8d1aee5
Zaid Almuflahi <zaid.almuflahi1217@gmail.com>
2022-10-12 11:26:06 -0400

    commit tree f0e45099ba6f97cf7c37fbfd5686f8493e41d3ca

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31 ;
+let num2 = 2 ;
+let multiply = num1 * num2 ;
 
+let random = Math.floor(Math.random() * 10) + 1
+
+
+let num3 = 22
+let num4 = 6
+let mod = Math.floor( num3 / num4)+ 1;
+
+let max = Math.max(16, 17, 18, 19, 20);
 // Write your code here
